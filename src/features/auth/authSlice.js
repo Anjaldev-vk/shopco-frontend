ca69c223d592d2ae46a31b2f2bc04760f2835999
AnjalDev <mkmanjal@gmail.com>
2026-02-20 22:41:52 +0530

    fix remaining import path

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import api from '../../services/delete_me';
+import api from '../../services/axiosBaseQuery';
 import { jwtDecode } from 'jwt-decode';
 
 /* =========================
