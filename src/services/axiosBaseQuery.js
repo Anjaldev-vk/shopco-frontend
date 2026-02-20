a8a69da5bcfe03d742bad70788a4a7d600b49c08
AnjalDev <mkmanjal@gmail.com>
2026-02-20 13:33:07 +0530

    shop.co_frontend_v4.2

--- src/services/axiosBaseQuery.js
+++ src/services/axiosBaseQuery.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://13.48.106.52",
   headers: {
     "Content-Type": "application/json",
   },
